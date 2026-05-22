b4d12e5a03d2c5a4fc96218bef4efbd975030e5f
Malu <maluaquino1007@gmail.com>
2026-05-22 15:28:43 -0300

    Adiconando o projeto

---
+++ src/test/java/br/com/fiap/gameapi/GameapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
